e3afde8c50779a84842806a38d50f5228bd2ea25
jvlimadev <jvlima_dev1999@outlook.com>
2022-11-03 23:10:14 -0300

    refactoring the code

--- nlw_copa/main.js
+++ nlw_copa/main.js
@@ -19,13 +19,9 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-      <img src="assets/logo.svg" alt="logo-copa">
-  </header>
-  <main id="cards">
-    ${createCard("24/11", "quinta", createGame('brazil', '16:00', 'serbia'))}
-    ${createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland'))}
-    ${createCard("02/12", "sexta", createGame('brazil', '16:00', 'cameroon'))}
-  </main>
-`
+document.querySelector("#cards").innerHTML =
+  
+  
+    createCard("24/11", "quinta", createGame('brazil', '16:00', 'serbia')) +
+    createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland')) +
+    createCard("02/12", "sexta", createGame('brazil', '16:00', 'cameroon')) 
